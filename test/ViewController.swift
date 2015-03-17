a933b036648d601bc59d7028c32102341ab0900a
Anneka <annecha17@gmail.com>
2015-03-17 17:55:20 +0900

    commitできないっ

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -17,6 +17,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
+        number = 1
         label.text = String(number)
     }
 
